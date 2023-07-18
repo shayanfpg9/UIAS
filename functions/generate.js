d2d3176692515d707e069a46ae88ae4bab5fdf0d
shayanfpg9 <shayanfpg9@duck.com>
2023-07-18 19:37:49 +0330

    make dynamic generate function

--- functions/generate.js
+++ functions/generate.js
@@ -2,9 +2,11 @@ const { randomBytes } = require("crypto");
 const Chance = require("chance");
 const chance = new Chance();
 
-function generate() {
-  const token = randomBytes(64).toString("hex").split("");
-  const hash = chance.hash({ length: 128 });
+function generate(length = 256) {
+  const token = randomBytes(length / 4)
+    .toString("hex")
+    .split("");
+  const hash = chance.hash({ length: length / 2 });
 
   return token.map((letter, index) => letter + hash[index]).join("");
 }
