6a104ecb9c9f17b902da915907448dd2f7414a8b
shayanfpg9 <shayanfpg9@duck.com>
2023-07-18 21:32:23 +0330

    get location with APIs

---
+++ functions/location.js
@@ -0,0 +1,34 @@
+const { default: axios } = require("axios");
+
+async function IP() {
+  const { ip } = (await axios.get("https://api64.ipify.org/?format=json")).data;
+  return ip;
+}
+
+async function location() {
+  const ip = await IP();
+  const geoLocation = (
+    await axios.get(
+      `https://ipinfo.io/${ip}/geo?token=${process.env["IPINFO-TOKEN"]}`
+    )
+  ).data;
+
+  return {
+    ...geoLocation,
+    location: geoLocation.loc.split(","),
+    toString: function () {
+      const removeSpace = (text) => text.replaceAll(" ", "-");
+      return (
+        removeSpace(this.city) +
+        "/" +
+        removeSpace(this.region) +
+        "/" +
+        removeSpace(this.country)
+      ).toLowerCase();
+    },
+  };
+}
+
+location.ip = IP;
+
+module.exports = location;
